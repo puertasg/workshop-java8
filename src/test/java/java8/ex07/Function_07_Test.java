4bbb46801fd595773b3d1b097f0e826e786d8919
Gauthier Puertas <gauthier.puertas@gmail.com>
2018-04-18 14:16:53 +0200

    Ex07 :  Défnition de substract

--- src/test/java/java8/ex07/Function_07_Test.java
+++ src/test/java/java8/ex07/Function_07_Test.java
@@ -32,7 +32,7 @@ public class Function_07_Test {
     }
 
     // TODO définir substract afin que le test test_format_subtract() soit passant
-    IntBinaryOperator substract = null;
+    IntBinaryOperator substract = (n1, n2) -> n1 - n2;
 
     @Test
     public void test_format_subtract() throws Exception {
